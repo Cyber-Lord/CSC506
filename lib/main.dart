fb37f5115184b195575367dfd700583d6868b2be
sagmam <mariamaliyu9@gmail.com>
2021-05-26 13:20:32 +0100

    changed color

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    );git
   }
 }
 
